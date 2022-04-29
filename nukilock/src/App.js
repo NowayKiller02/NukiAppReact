a953fb88767d919562e401c2fe685b62e32da421
LiemTech <hans.naert@vives.be>
2022-04-29 15:37:04 +0200

    Liem - Logo + 2 buttons

--- nukilock/src/App.js
+++ nukilock/src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import logo from './NukiLogo.png';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      Hello world!
+      <br></br>
+      <img src= {logo} alt="Logo Vives Nukilock" id = "logo" width = "623" height = "194"></img>
+      <br></br><br></br><br></br>
+      <button class="button-31" role="button">LOCK</button>
+      <br></br>
+      <button class="button-31" role="button">UNLOCK</button>
     </div>
   );
 }
