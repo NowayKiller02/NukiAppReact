2dea03f1dda5b46fe20822778b7b6bc6f7571a60
NowayKiller02 <yuran.loones@student.vives.be>
2022-05-06 15:37:09 +0200

    Post via fetch, UI, pswFile -> yuran
    Yuran

--- nukilock/src/App.js
+++ nukilock/src/App.js
@@ -1,22 +1,62 @@
 import * as React from 'react';
 import logo from './NukiLogo.png';
 import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
 import './App.css';
-import 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap';
-import 'https://fonts.googleapis.com/icon?family=Material+Icons';
 
+const token = "bdeb6ae900e63ad6e8c13afa19fa2ce4b053838c7d1efd91cddc209b95b6acec74740b2c3602946e";
+
+const Openurl = "https://api.nuki.io/smartlock/645574324/action/unlock";
+const Closeurl = "https://api.nuki.io/smartlock/645574324/action/lock";
+
+
+function OpenLock() {
+  console.log("Opening Lock");
+  fetch(Openurl, {
+    method: 'post',
+    headers: new Headers({
+      'Authorization': `Bearer ${token}`,
+      // 'Content-Type': 'application/x-www-form-urlencoded'
+    })
+  }).then(res => {
+    console.log(`statusCode: ${res.status}`)
+    console.log(res)
+  })
+  .catch(error => {
+    console.error(error)
+  });
+};
+function CloseLock() {
+  console.log("Closing Lock");
+  fetch(Closeurl, {
+    method: 'post',
+    headers: new Headers({
+      'Authorization': `Bearer ${token}`,
+      // 'Content-Type': 'application/x-www-form-urlencoded'
+    }),
+  }).then(res => {
+    console.log(`statusCode: ${res.status}`)
+    console.log(res)
+  })
+  .catch(error => {
+    console.error(error)
+  });
+};
 
 
 function App() {
   return (
     <div className="App">
+      <Paper  square>
+        <img src={logo} alt='logo' />
+      </Paper>
       <br></br>
-      <img src= {logo} alt="Logo Vives Nukilock" id = "logo" width = "623" height = "194"></img>
-      <br></br><br></br><br></br>
-      <Button variant="contained">Lock</Button>;
       <br></br>
-      <Button variant="contained">Unlock</Button>;
+      <Button onClick={CloseLock} style={{ height: '75px', width: '45%' }} variant="outlined" color="success">Lock</Button>
+      <Button onClick={OpenLock} style={{ height: '75px', width: '45%' }} variant="outlined" color="error">Unlock</Button>
     </div>
   );
 }
 export default App;
+
+
